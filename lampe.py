0e8f31007e59e42b397c933afbcd33da6194a1de
jasonb315 <jasonb315@protonmail.com>
2018-12-17 12:38:22 -0800

    ini w/repl.it reansfer

---
+++ lampe.py
@@ -0,0 +1,53 @@
+
+
+
+# def letterBlock(phrase, offset, interval):
+#     sentence = phrase
+#     words = sentence.split()
+#     output = ''
+#     lettercount = 0
+#     returnString = ''
+
+#   for word in words:
+#     for letter in word:
+#       lettercount += 1
+#       output += (chr(ord(letter)+offset))
+#     output += ' '
+
+#   returnString += output + "\n"
+#   sentence = output
+#   words = sentence.split()
+#   output = ''
+
+#   for k in range(51):
+#     for word in words:
+#       for letter in word:
+#         output += (chr(ord(letter)+interval))
+#       output += ' '
+
+#     returnString += output + "\n"
+#     sentence = output
+#     words = sentence.split()
+#     output = ''
+  
+#   return returnString
+
+# # print(letterBlock("J A S O N", 0, 1))
+# # letterBlock("B U R N S", 0, 1)
+# # letterBlock("0 1 2 3 4 5 6 7 8 9", 1, -1)
+# print(letterBlock("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z Y X W V U T S R Q P O N M L K J I H G F E D C B A", -10, 1))
+# # letterBlock("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 0, -1)
+# # undo = output.split()
+# # undoprint = ''
+# print(len("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z Y X W V U T S R Q P O N M L K J I H G F E D C B A"))
+# # for letter in undo:
+# #   undoprint += ' '
+# #   for x in letter:
+# #     undoprint += (chr(ord(x)-1111))
+  
+# # print(undoprint)
+
+# # def break_line():
+# #   return "line\nbreak"
+# # print(break_line())
+
